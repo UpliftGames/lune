80961a664e81402aa5638498f7c0e59588e59a5b
Micah <micah@uplift.games>
2025-04-29 09:37:57 -0700

    Use forked version of rbx-dom

--- crates/lune-roblox/src/reflection/mod.rs
+++ crates/lune-roblox/src/reflection/mod.rs
@@ -134,7 +134,7 @@ impl LuaUserData for Database {
 
 impl Default for Database {
     fn default() -> Self {
-        Self(rbx_reflection_database::get())
+        Self(rbx_reflection_database::get().unwrap())
     }
 }
 
